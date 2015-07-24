f64331cb7a93c1c60c693cb3abf92320d1fd1a1a
evildemon0s <evil.demon_s@yahoo.com>
2015-07-25 01:37:24 +0530

    Add Xperia Genesis Banner and Team Startup

---
+++ src/com/android/settings/xperia/Team.java
@@ -0,0 +1,51 @@
+package com.android.settings.xperia;
+import android.content.ContentResolver;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.widget.Toast;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class Team extends SettingsPreferenceFragment implements
+Preference.OnPreferenceChangeListener {
+private static final String TAG = "XperiaGenesisTeam";
+Preference mEvild;
+@Override
+public void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+ContentResolver resolver = getActivity().getContentResolver();
+// Load the preferences from an XML resource
+addPreferencesFromResource(R.xml.genesis_team);
+PreferenceScreen prefSet = getPreferenceScreen();
+mEvild = prefSet.findPreference("genesis_evil");
+}
+@Override
+public void onResume() {
+super.onResume();
+}
+@Override
+public void onDestroy() {
+super.onDestroy();
+}
+@Override
+public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+ContentResolver resolver = getActivity().getContentResolver();
+boolean value;
+if (preference == mEvild) {
+Toast.makeText(getActivity(), "Owner Of Xperia Genesis, Loves Making Xperia Themed Roms",
+Toast.LENGTH_LONG).show();
+} else {
+return super.onPreferenceTreeClick(preferenceScreen, preference);
+}
+return true;
+}
+@Override
+public boolean onPreferenceChange(Preference preference, Object objValue) {
+ContentResolver resolver = getActivity().getContentResolver();
+final String key = preference.getKey();
+return true;
+}
+}
